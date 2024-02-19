7ffe1948917fc31e5ffb59fdb66a900995a0021b
High23 <warenasb2@gmail.com>
2024-02-18 23:16:55 -0700

    Pokemon images now are shown/fetched by given id

--- src/App.jsx
+++ src/App.jsx
@@ -47,10 +47,58 @@ function App() {
     <>
       <header>Memory Card Project</header>
       <main>
+        {(randomizedPokemonIds.length > 0 && pokemonIds.length > 0) && 
+        <>
+          <PokemonImages setPokemonRefresh={setPokemonRefresh} randomPokemonId={randomizedPokemonIds[0]}></PokemonImages>
+          <PokemonImages setPokemonRefresh={setPokemonRefresh} randomPokemonId={randomizedPokemonIds[1]}></PokemonImages>
+          <PokemonImages setPokemonRefresh={setPokemonRefresh} randomPokemonId={randomizedPokemonIds[2]}></PokemonImages>
+          <PokemonImages setPokemonRefresh={setPokemonRefresh} randomPokemonId={randomizedPokemonIds[3]}></PokemonImages>
+          <PokemonImages setPokemonRefresh={setPokemonRefresh} randomPokemonId={randomizedPokemonIds[4]}></PokemonImages>
+          <PokemonImages setPokemonRefresh={setPokemonRefresh} randomPokemonId={randomizedPokemonIds[5]}></PokemonImages>
+          <PokemonImages setPokemonRefresh={setPokemonRefresh} randomPokemonId={randomizedPokemonIds[6]}></PokemonImages>
+          <PokemonImages setPokemonRefresh={setPokemonRefresh} randomPokemonId={randomizedPokemonIds[7]}></PokemonImages>
+          <PokemonImages setPokemonRefresh={setPokemonRefresh} randomPokemonId={randomizedPokemonIds[8]}></PokemonImages>
+          <PokemonImages setPokemonRefresh={setPokemonRefresh} randomPokemonId={randomizedPokemonIds[9]}></PokemonImages>
+          <PokemonImages setPokemonRefresh={setPokemonRefresh} randomPokemonId={randomizedPokemonIds[10]}></PokemonImages>
+          <PokemonImages setPokemonRefresh={setPokemonRefresh} randomPokemonId={randomizedPokemonIds[11]}></PokemonImages>
+          </>
+        }
       </main>
       <footer></footer>
     </>
   )
+}
+
+function PokemonImages(props) {
+  const [imageUrls, setImageUrls] = useState()
+  const [pokemonName, setPokemonName] = useState()
+  const imageOne = fetchPokemonById(props.randomPokemonId)
+  useEffect(() => {
+    imageOne.then(result => {
+      console.log(result.sprites.front_default)
+      setImageUrls(result.sprites.other.dream_world.front_default)
+      setPokemonName(result.forms[0].name)
+    }).catch(err => {
+      console.log(`here's the error: ${err}`)
+    })
+  }, [])
+  
+  return (
+    <>
+      <div>
+        <button type="button" >
+          <img src={imageUrls} alt=""></img>
+          <div>{pokemonName}</div>
+          </button>
+      </div>
+    </>
+  )
+}
+
+async function fetchPokemonById(id) {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+    const data = await response.json()
+    return data
 }
 
 export default App
