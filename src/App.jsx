147f1e111e52a73d2810a7266e82e526f079da50
High23 <warenasb2@gmail.com>
2024-02-20 23:43:14 -0700

    Improved fetch requests and pokemon randomization
    -Fetching pokemon data use to make api calls every
    rerender which would result in too many
    unnecessary api calls, changed it to only request
    one set and only call the api when the entire set
    of pokemon needed to be refreshed.
    
    -Pokemon are now randomized after they are called
    by the api and not before

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
+import pikachuGif from './assets/pikachu-running.gif'
 import './App.css'
 
 function App() {
@@ -19,49 +19,20 @@ function App() {
       }
     }
     setPokemonIds([...idSet])
-    console.log(idSet)
     return () => {
       setPokemonIds([])
     }
   }, [pokemonRefresh])
-
-  const [randomizedPokemonIds, setRandomizedPokemonIds] = useState([])
-  const [pokemonIdRefresh, setPokemonIdRefresh] = useState(false)
-  useEffect(() => {
-    let pokemonIdsCopy = pokemonIds
-    let randomizedIds = []
-    while (pokemonIdsCopy.length > 0) {
-      let randomNumber = Math.floor(Math.random() * Number(pokemonIdsCopy.length))
-      randomizedIds.length > 0 ? randomizedIds = [...randomizedIds, pokemonIdsCopy[randomNumber]] : randomizedIds = [pokemonIdsCopy[randomNumber]]
-      pokemonIdsCopy = pokemonIdsCopy.filter((id) => id !== pokemonIdsCopy[randomNumber])
-    }
-    console.log(randomizedIds)
-    setRandomizedPokemonIds([...randomizedIds])
-    return () => {
-      setRandomizedPokemonIds([])
-    }
-  }, [pokemonIds, pokemonIdRefresh])
 
 
   return (
     <>
       <header>Memory Card Project</header>
       <main>
-        {(randomizedPokemonIds.length > 0 && pokemonIds.length > 0) && 
+        {pokemonIds.length > 0  && 
         <>
-          <PokemonImages setPokemonRefresh={setPokemonRefresh} randomPokemonId={randomizedPokemonIds[0]}></PokemonImages>
-          <PokemonImages setPokemonRefresh={setPokemonRefresh} randomPokemonId={randomizedPokemonIds[1]}></PokemonImages>
-          <PokemonImages setPokemonRefresh={setPokemonRefresh} randomPokemonId={randomizedPokemonIds[2]}></PokemonImages>
-          <PokemonImages setPokemonRefresh={setPokemonRefresh} randomPokemonId={randomizedPokemonIds[3]}></PokemonImages>
-          <PokemonImages setPokemonRefresh={setPokemonRefresh} randomPokemonId={randomizedPokemonIds[4]}></PokemonImages>
-          <PokemonImages setPokemonRefresh={setPokemonRefresh} randomPokemonId={randomizedPokemonIds[5]}></PokemonImages>
-          <PokemonImages setPokemonRefresh={setPokemonRefresh} randomPokemonId={randomizedPokemonIds[6]}></PokemonImages>
-          <PokemonImages setPokemonRefresh={setPokemonRefresh} randomPokemonId={randomizedPokemonIds[7]}></PokemonImages>
-          <PokemonImages setPokemonRefresh={setPokemonRefresh} randomPokemonId={randomizedPokemonIds[8]}></PokemonImages>
-          <PokemonImages setPokemonRefresh={setPokemonRefresh} randomPokemonId={randomizedPokemonIds[9]}></PokemonImages>
-          <PokemonImages setPokemonRefresh={setPokemonRefresh} randomPokemonId={randomizedPokemonIds[10]}></PokemonImages>
-          <PokemonImages setPokemonRefresh={setPokemonRefresh} randomPokemonId={randomizedPokemonIds[11]}></PokemonImages>
-          </>
+          <PokemonImages setPokemonRefresh={setPokemonRefresh} pokemonIds={pokemonIds}></PokemonImages>
+        </>
         }
       </main>
       <footer></footer>
@@ -69,36 +40,92 @@ function App() {
   )
 }
 
-function PokemonImages(props) {
-  const [imageUrls, setImageUrls] = useState()
-  const [pokemonName, setPokemonName] = useState()
-  const imageOne = fetchPokemonById(props.randomPokemonId)
+function PokemonImages({setPokemonRefresh, pokemonIds}) {
+  const [pokemonData, setPokemonData] = useState([])
+  const [randomizedPokemonData, setRandomizedPokemonData] = useState([])
+  const [pokemonIdRefresh, setPokemonIdRefresh] = useState(false)
+  const [state, setState] = useState('Loading')
+ 
   useEffect(() => {
-    imageOne.then(result => {
-      console.log(result.sprites.front_default)
-      setImageUrls(result.sprites.other.dream_world.front_default)
-      setPokemonName(result.forms[0].name)
-    }).catch(err => {
-      console.log(`here's the error: ${err}`)
-    })
-  }, [])
+    setState('Loading')
+    fetchPokemonById(pokemonIds).then(data => {
+      setPokemonData(data)
+    }).catch(err => console.log(err))
+    return () => {
+      setPokemonData([])
+    }
+  }, [pokemonIds]) 
+
+  useEffect(() => {
+    let pokemonDataCopy = pokemonData
+    let randomizedIds = []
+    while (pokemonDataCopy.length > 0) {
+      let randomNumber = Math.floor(Math.random() * Number(pokemonDataCopy.length))
+      randomizedIds.length > 0 ? randomizedIds = [...randomizedIds, pokemonDataCopy[randomNumber]] : randomizedIds = [pokemonDataCopy[randomNumber]]
+      pokemonDataCopy = pokemonDataCopy.filter((id) => id !== pokemonDataCopy[randomNumber])
+    }
+    console.log(randomizedIds)
+    setRandomizedPokemonData([...randomizedIds])
+    return () => {
+      setRandomizedPokemonData()
+    }
+  }, [pokemonData, pokemonIdRefresh])
+
+  
   
   return (
     <>
-      <div>
-        <button type="button" >
-          <img src={imageUrls} alt=""></img>
-          <div>{pokemonName}</div>
+      {state === 'Loading' && 
+      <div className='loading'> 
+        <img src={pikachuGif} alt=""></img> 
+        <div>Loading...</div>
+      </div>}
+      {(pokemonData.length > 0) &&
+        <DisplayPokemon setPokemonIdRefresh={setPokemonIdRefresh} randomizedPokemonData={randomizedPokemonData} pokemonIdRefresh={pokemonIdRefresh} setState={setState} state={state}></DisplayPokemon>
+      }
+    </>
+  )
+}
+
+
+function DisplayPokemon({setPokemonIdRefresh, randomizedPokemonData, pokemonIdRefresh, setState, state}) {
+  useEffect(() => {
+    const load = setInterval(() => {
+      setState('Success')
+    }, 2000); 
+    return () => {
+      clearInterval(load)
+    }
+  })
+
+  return (
+    <>
+    {state === 'Success' && randomizedPokemonData.map((pokemon) => {
+      return (
+        <div key={pokemon.id}>
+          <button type="button" onClick={() => {
+            console.log(pokemonIdRefresh)
+            pokemonIdRefresh ? setPokemonIdRefresh(false) : setPokemonIdRefresh(true)
+            }}>
+            <img src={pokemon.sprites.other.dream_world.front_default} alt=""></img>
+            <div>{pokemon.forms[0].name.charAt(0).toUpperCase() + pokemon.forms[0].name.slice(1)}</div>
           </button>
-      </div>
+        </div>
+      )
+    })}
     </>
   )
 }
 
 async function fetchPokemonById(id) {
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+  let dataArray = [];
+  for (let i = 0; i < 12; i++){
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id[i]}`)
+    console.log(response)
     const data = await response.json()
-    return data
+    dataArray.push(data)
+  }
+  return dataArray
 }
 
 export default App
